d5653533d65186621451f67f93cd18b5cd1207dc
Nguyen Trung Tai - TJ <tai.nguyentrung1996@gmail.com>
2022-12-21 10:35:45 +0700

    Build a basic app Age Teller Application

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
+import "./App.css";
+
+import AgeStats from "./AgeStats";
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			newDate: "",
+			birthday: this.getCurrentDate(),
+		};
+	}
+
+	changeBirthday() {
+		this.setState({
+			birthday: this.state.newDate,
+		});
+	}
+
+	getCurrentDate() {
+		const d = new Date();
+		const currentTZ = new Intl.DateTimeFormat("en-GB");
+		return currentTZ.format(d);
+	}
+
+	render() {
+		return (
+			<>
+				<Form inline className="App">
+					<h2>Input your Birthday</h2>
+					<FormControl
+						type="date"
+						onChange={(e) => this.setState({ newDate: e.target.value })}
+					></FormControl>{" "}
+					<br />
+					<Button variant="light" onClick={() => this.changeBirthday()}>
+						Submit
+					</Button>
+					<AgeStats date={this.state.birthday} />
+				</Form>
+			</>
+		);
+	}
+}
+
+export default App;
